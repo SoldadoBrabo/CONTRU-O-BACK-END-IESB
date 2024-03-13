254bb7f93d72dcccbc7aaf086fc14ce53885ea96
SoldadoBrabo <igor00133t@gmail.com>
2024-03-13 20:19:46 -0300

    ignore

---
+++ fundamentos1/variaveis.js
@@ -0,0 +1,16 @@
+//Criando variaveis com var 
+var nomeCompleto
+
+//atribuição na variavel nomeCompleto(=)
+nomeCompleto = "Igor Vieira Peixoto"
+
+//console.log("Olá ", nomeCompleto)
+
+var primeiroNome = "Igor "
+var nomeDoMeio = "Vierira "
+var ultimoNome = "Peixoto "
+var idade = "35"
+
+
+console.log("NOME:". primeiroNome + "" + nomeDoMeio + "" + ultimoNome)
+console.log("IDADE: " + idade)
